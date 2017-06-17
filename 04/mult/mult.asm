4b63f8fd8b8eb2504e1ea5790160281b126a9b15
Akhand <apratap.mishra.cer15@itbhu.ac.in>
2017-06-17 11:28:09 +0530

    ntt

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,37 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@i 
+M=1 //i=1
+@R2
+M=0	//R2=R2+R1
+
+(LOOP)
+	@i
+	D=M	//D=I
+	@R0
+	D=D-M //D=D-R0
+	@END
+	D;JGT //IF(D>0) GOTO END
+	
+	@R1
+	D =M //D=R1
+	@R2
+	M=M+D //R2+=R1
+	
+	@i
+	M=M+1	//I=I+1
+	@LOOP
+	0;JMP	//GOTO LOOP
+
+(END)
+	@END
+	0;JMP
+
+
+
+
+
+
+
